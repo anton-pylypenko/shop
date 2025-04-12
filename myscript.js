99e7828120769d26e916758dfc6d48865bf2a3aa
Anton Pylypenko <86245132+anton-pylypenko@users.noreply.github.com>
2025-04-12 12:13:11 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,4 @@
+document.getElementById('1').src = 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg';
+document.getElementById('2').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2mpnEBam3dC0D8wNKLCjE8jJJkj5zEF57xHKp4A57yQ&s';
+document.getElementById('3').src = 'https://foto.kontan.co.id/ACDo9Bw1vLMDoKU3ry6ftHH_fnY=/smart/filters:format(webp)/2024/01/18/243341218p.jpg';
+document.getElementById('4').src = 'https://images.prom.ua/5358400525_w600_h600_5358400525.jpg';
